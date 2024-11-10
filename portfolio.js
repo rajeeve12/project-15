a879052cc659ad2f6ca2f226da42a89b21ed4f63
rajeeve12 <rajeevevishen21@gmal.com>
2024-11-10 11:04:27 -0500

    Create portfolio module with calculatePortfolioValue and getPortfolioAllocation functions.

---
+++ portfolio.js
@@ -0,0 +1,18 @@
+import { assets } from './asset';
+const calculatePortfolioValue = () => {
+    return assets.reduce((total, asset) => {
+        return total + (asset.price * asset.quantity);
+    }, 0);
+};
+
+const getPortfolioAllocation = () => {
+    const totalValue = calculatePortfolioValue();
+    
+    return assets.map(asset => {
+        const assetValue = asset.price * asset.quantity;
+        const allocation = (assetValue / totalValue) * 100;)
+        
+        return {id: asset.id, name: asset.name, type: asset.type, value: assetValue, 
+            allocation: Number(allocation.toFixed()) 
+        }};
+        export { calculatePortfolioValue, getPortfolioAllocation };
